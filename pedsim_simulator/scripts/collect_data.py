0d465749233d566018323124e10634f66b99f5dc
linsea94 <linsea94@gmail.com>
2022-09-01 23:23:52 +0800

    add collect_data rate and SplitData ver.1

--- pedsim_simulator/scripts/collect_data.py
+++ pedsim_simulator/scripts/collect_data.py
@@ -29,7 +29,7 @@ home = os.path.expanduser("~")
 
 FIRST_TIME = True
 file_name = rospy.get_param("/collect_data/file_name")
-file_path = home +'/motion_ws/src/pedsim_ros/data_process/datas/'+file_name+'.csv'
+file_path = home +'/motion_ws/src/pedsim_ros/data_process/datas/split_data/'+file_name+'.csv'
 
 
 def save_agent_info(agents_info):
@@ -38,15 +38,34 @@ def save_agent_info(agents_info):
     agents_lst=[]
 
     for item in agents_info.agent_states:
-        agent_info = [agents_info.header.seq, item.id, item.pose.position.x, item.pose.position.z, 
-                        item.pose.position.y, item.twist.linear.x, item.twist.linear.z, item.twist.linear.y]
-        agents_lst.append(agent_info)
+        
+        #make time
+        l_nsecs = len(str(agents_info.header.stamp.nsecs))
+        if(9-l_nsecs):
+            nsecs = str(0)* (9-l_nsecs)+ str(agents_info.header.stamp.nsecs)
+        else:
+            nsecs = str(agents_info.header.stamp.nsecs)
+        time = np.round(float(str(agents_info.header.stamp.secs) + '.' + nsecs), 4)
+        
+        #rate (2.5)
+        if (time % 0.4) < 0.0999:
+            # agent_info = [ time, item.id, item.pose.position.x, item.pose.position.z, 
+            #                 item.pose.position.y, item.twist.linear.x, item.twist.linear.z, item.twist.linear.y]
+            agent_info = [ time, item.id, item.pose.position.x, item.pose.position.y, item.twist.linear.x, item.twist.linear.y]
+            agents_lst.append(agent_info)
 
-    df2 = pd.DataFrame(agents_lst)
-    df2.columns = ['frame_number', 'ID', 'pos_x', 'pos_z', 'pos_y', 'vel_x', 'vel_z', 'vel_y']
-    
-    with open(file_path,mode= 'a') as f:
-        df2.to_csv(f, header = False, index = False)
+    try:
+        df2 = pd.DataFrame(agents_lst)
+        df2.columns = ['time', 'ID', 'pos_x', 'pos_z', 'pos_y', 'vel_x', 'vel_z', 'vel_y']
+
+        with open(file_path,mode= 'a') as f:
+            if FIRST_TIME:
+                df2.to_csv(f, header = True, index = False)
+                FIRST_TIME = False
+            else:
+                df2.to_csv(f, header = False, index = False)
+    except:
+        pass
 
 
 def listener(agent_info):
@@ -64,6 +83,7 @@ def listener(agent_info):
 if __name__ == '__main__':
 
     rospy.init_node('collect_data', anonymous=True)
+    # rate = rospy.Rate(2.5)
 
     agents_info = '/pedsim_simulator/simulated_agents'
 
